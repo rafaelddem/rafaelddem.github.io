49eef46b16eebd3291430eb216c46c411aa27a86
rafaelddem <42066475+rafaelddem@users.noreply.github.com>
2022-11-09 14:56:41 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "
+<h1>Test Page</h1>
+
+<h2>Content um php</h2>
+
+<p>
+Sagittis purus sit amet volutpat consequat mauris nunc. Et leo duis ut diam quam nulla porttitor massa id. In egestas erat imperdiet sed. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Etiam erat velit scelerisque in dictum non. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu. Eget nullam non nisi est. Amet luctus venenatis lectus magna fringilla urna. Eget felis eget nunc lobortis mattis aliquam faucibus. Egestas fringilla phasellus faucibus scelerisque. Duis convallis convallis tellus id interdum velit laoreet. Porttitor eget dolor morbi non arcu risus. Augue ut lectus arcu bibendum at varius vel. Donec et odio pellentesque diam volutpat commodo. Cursus in hac habitasse platea dictumst. At imperdiet dui accumsan sit amet nulla facilisi. Purus ut faucibus pulvinar elementum integer enim. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin. In aliquam sem fringilla ut morbi.
+";
